919d84c659ae3a90dcc96e4f65ec9aa5b64a5101
RansomBroker <yadisoke@gmail.com>
2023-07-07 11:11:38 +0700

    membuat template

---
+++ resources/views/includes/css.blade.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
+
+<style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+    }
+
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
+</style>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>@yield('title')</title>
+    @include('includes.css')
+</head>
+<body>
+
+@include('includes.navbar')
+
+<div class="container-fluid">
+    <div class="row">
+        @include('includes.sidebar')
+
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            @yield('content')
+        </main>
+    </div>
+</div>
+
+
+@include("includes.js")
+</body>
+</html>
