4db8299906b6d25ae44ae15c51436d33a78d77ff
RansomBroker <yadisoke@gmail.com>
2023-07-07 17:30:46 +0700

    belajar API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get("/category/get", [CategoryController::class, "categoryDataGet"]);
+Route::post("/category/add", [CategoryController::class, "categoryDataAdd"]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
