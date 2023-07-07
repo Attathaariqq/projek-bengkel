7ba0727f49d52eb898ba638a979b431f660d58a3
RansomBroker <yadisoke@gmail.com>
2023-07-07 12:49:42 +0700

    menambahkan method pada ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function get()
+    {
+
+    }
+
+    public function addView()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function editView()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
 }
